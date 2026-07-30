0270a4bbe005c8eba0310699f5cdae4bd3a65941
Nick Rawcliffe <nrawcdev@gmail.com>
2026-07-30 21:04:01 +0100

    add title color

--- lua/colors/astigmatism.lua
+++ lua/colors/astigmatism.lua
@@ -89,6 +89,7 @@ set(0, "Special",          { fg = colors.orange })
 set(0, "WarningMsg",       { fg = colors.warning })
 set(0, "Error",            { fg = colors.error })
 set(0, "Operator",         { fg = colors.keyword})
+set(0, "Title",            { fg = colors.keyword})
 
 -- Diagnostics
 set(0, "DiagnosticError",  { fg = colors.red })
